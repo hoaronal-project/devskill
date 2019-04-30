2be3f6173dd1d087e55113858358fddaf423603a
Quang Hoa <hoa9x3@gmail.com>
2019-04-30 22:46:01 +0700

    change icon login

--- src/main/java/com/techblog/listener/StartedListener.java
+++ src/main/java/com/techblog/listener/StartedListener.java
@@ -1,21 +1,33 @@
 package com.techblog.listener;
 
 import com.techblog.common.constant.Constants;
+import com.techblog.domain.Tag;
+import com.techblog.service.tag.TagService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.beanutils.BeanToPropertyValueTransformer;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @Configuration
 public class StartedListener implements ApplicationListener<ApplicationStartedEvent> {
 
+    private final TagService tagService;
+
+    public StartedListener(TagService tagService) {
+        this.tagService = tagService;
+    }
+
     @Override
     public void onApplicationEvent(ApplicationStartedEvent event) {
-        System.out.println("hoa hahaha");
-        Constants.TAGS = Arrays.asList("Java","Php", "C#");
-        log.info("The scheduled task starts successfully!");
+        log.info("Load data ===========>>>");
+        List<Tag> tags = tagService.findAll();
+        Constants.TAGS = (List<String>) CollectionUtils.collect(tags,
+                new BeanToPropertyValueTransformer("name"));
+
     }
 }
